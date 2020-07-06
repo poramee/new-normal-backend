4185777bd5b3f8e30670cba9f306fa575e18b608
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2020-07-06 14:31:38 +0700

    change response message format

--- functions/functions/places.js
+++ functions/functions/places.js
@@ -8,6 +8,7 @@ exports.getInfo = (request, response, database) => {
 		.doc(request.params.id)
 		.get()
 		.then((snapshot) => {
+			console.log(typeof snapshot.data());
 			if (!snapshot.exists)
 				response.status(404).send("The requested place id is not found.");
 			else response.status(200).send(snapshot.data());
@@ -25,7 +26,17 @@ const recordActivity = async (request, response, database, type) => {
 		.get()
 		.then(async (snapshot) => {
 			if (snapshot.exists) {
+				const data = snapshot.data();
 				const currentTime = new Date();
+				if (data.visitor_number <= 0 && type === "exit") {
+					const responseToSend = {
+						status: "406 Error",
+						message: "Current capacity is now 0",
+					};
+					response.status(406).send(responseToSend);
+					return;
+				}
+
 				await database
 					.collection("places")
 					.doc(
@@ -39,7 +50,19 @@ const recordActivity = async (request, response, database, type) => {
 						date: firestore.firestore.Timestamp.fromDate(currentTime),
 						type: type,
 					});
-				response.status(200).send("Success");
+				await database
+					.collection("places")
+					.doc(request.params.id)
+					.update({
+						visitor_number: firestore.firestore.FieldValue.increment(
+							type === "exit" ? -1 : 1
+						),
+					});
+				const responseToSend = {
+					status: "Success",
+					message: null,
+				};
+				response.status(200).send(responseToSend);
 			} else response.status(404).send("The requested place id is not found.");
 			return;
 		})
@@ -57,35 +80,42 @@ exports.recordPersonExit = async (request, response, database) => {
 };
 exports.createNewPlace = async (request, response, database) => {
 	const data = request.body;
-	let responseMessage = "";
+	let responseMessage = [];
 	if (!data.hasOwnProperty("name"))
-		responseMessage += "'name' field is not found.\n";
+		responseMessage.push("'name' field is not found.");
 
 	if (!data.hasOwnProperty("category"))
-		responseMessage += "'category' field is not found.\n";
+		responseMessage.push("'category' field is not found.");
 
 	if (!data.hasOwnProperty("location"))
-		responseMessage += "'location' field is not found.\n";
+		responseMessage.push("'location' field is not found.");
 	else {
 		if (!data.location.hasOwnProperty("latitude"))
-			responseMessage +=
-				"'latitude' field in property 'location' is not found.\n";
+			responseMessage.push(
+				"'latitude' field in property 'location' is not found."
+			);
 		else if (typeof data.location.latitude !== "number")
-			responseMessage += "'latitude' field must be a number.\n";
+			responseMessage.push("'latitude' field must be a number.");
 		if (!data.location.hasOwnProperty("longitude"))
-			responseMessage +=
-				"'longitude' field in property 'location' is not found.\n";
+			responseMessage.push(
+				"'longitude' field in property 'location' is not found."
+			);
 		else if (typeof data.location.longitude !== "number")
-			responseMessage += "'longitude' field must be a number.\n";
+			responseMessage.push("'longitude' field must be a number.");
 	}
 	if (!data.hasOwnProperty("capacity"))
-		responseMessage += "'capacity' field is not found.\n";
+		responseMessage.push("'capacity' field is not found.");
 	else if (typeof data.capacity !== "number")
-		responseMessage += "'capacity' field must be a number.\n";
+		responseMessage.push("'capacity' field must be a number.");
 
-	if (responseMessage.length > 0) response.status(406).send(responseMessage); // If there's an error
+	if (responseMessage.length > 0)
+		response
+			.status(406)
+			.send({ status: "406 Error", message: responseMessage });
+	// If there's an error
 	else {
-		const dataToStore = {
+		try{
+			const dataToStore = {
 			name: data.name.toString(),
 			category: data.category.toString(),
 			location: new firestore.firestore.GeoPoint(
@@ -93,16 +123,33 @@ exports.createNewPlace = async (request, response, database) => {
 				data.location.longitude
 			),
 			capacity: data.capacity,
-			vistor_number: 0,
+			visitor_number: 0,
 		};
-		database.collection("places").add(dataToStore)
-			.then(docRef => {
-				response.status(200).send("Success\ndoc id: " + docRef.id.toString() + "\n" + JSON.stringify(dataToStore));
+		database
+			.collection("places")
+			.add(dataToStore)
+			.then((docRef) => {
+				const responseToSend = {
+					status: "Success",
+					message: {
+						documentID: docRef.id.toString(),
+						storedData: dataToStore,
+					},
+				};
+				response.status(200).send(responseToSend);
 				return;
 			})
-			.catch(error => {
+			.catch((error) => {
+				const responseToSend = {
+					status: "Error 500",
+					message: error.toString(),
+				};
 				console.log(error);
-				response.status(500).send("500 Internal Server Error\n" + error.toString());
-			})
+				response.status(500).send(responseToSend);
+			});
+		} catch (error){
+			response.status(406).send({status: "406 Error",message: error.toString()})
+		}
+		
 	}
 };
